583039f0fae48bcda1bc3b4ff4da672b4ca46c16
adamhirata <35279376+adamhirata@users.noreply.github.com>
2020-02-08 22:58:48 -0800

    Add files via upload

--- syntax/__tests__/grammar.test.js
+++ syntax/__tests__/grammar.test.js
@@ -9,8 +9,8 @@ const syntaxCheck = require('../syntax-checker');
 
 const program = String.raw`
     Stwing a () uwu 
-        Numbwer a = 1               (*≧ω≦*) this is a comment
-        Numbwer b = 1 + 1
+        Numbwer a = 1               (ﾉﾟ▽ﾟ)ﾉ           this is a comment
+        Numbwer b = 1 + 1           (づ ◕‿◕ )づ     All binary operators + assignment
         Numbwer c = 1 - 1
         Numbwer d = 1 * 1
         Numbwer e = 1 / 1
@@ -21,14 +21,39 @@ const program = String.raw`
         Boow    j = 1 >= 1
         Boow    k = 1 <= 1
         Boow    l = 1 != 1
+        Boow    m = twue
+
+        Numbwer n = -1              (*≧ω≦*)           Unary operators
+        Boow    o = !fawse
+
+        Dictionawy< Stwing to Boow >    p = { a to 2, b to twue }       (ﾉﾟ▽ﾟ)ﾉ       Dictionary and Array types
+        Awway< Awway<Stwing> >          q = [["hi"], ["dr"], ["Toal"], [":)"]]
+
+        Stwing  r = null            (づ ◕‿◕ )づ         The million-dollar mistake
         retuwn "Stwing"
     owo
+    Stwing b () uwu retuwn "hi" owo     (*≧ω≦*)     Single-line function declaration
 
-    Stwing b () uwu retuwn "Stwing" owo
+    wile (1) uwu pwint("hi") owo        (ﾉﾟ▽ﾟ)ﾉ     While-statements
+    wile (1) uwu
+        pwint("hi")
+        pwint("Prof Toal")
+    owo
+    wile (1) uwu owo
 
-    void c () uwu
-        Stwing b = "Stwing"         (*≧ω≦*) notice the optional owo at the end of blocks
+    if (1 < 1) uwu                      (づ ◕‿◕ )づ     If, else-if, else
+        retuwn
+    owo ewse if (1 > 1) uwu
+    owo ewse uwu pwint("hi") owo
 
+    fow i in 0...Len(Awway) uwu         (*≧ω≦*)         For-statements
+        fow j in 0...Len(Awway) uwu
+            pwint(Awway[i][j])
+        owo
+    owo
+
+    pwint("hi")
+    
     (╯°益°)╯彡┻━┻
         this
         is
