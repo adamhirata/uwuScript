7d33f7ec7779e3a6591e8d12e6e35d97eea85b91
Adam Hirata <takahiro.adam@gmail.com>
2020-03-07 23:22:46 -0800

    Cleaning up unused code

--- ast/graveyard/argument.js
+++
@@ -1,5 +0,0 @@
-module.exports = class Argument {
-  constructor(id, expression) {
-    Object.assign(this, { id, expression });
-  }
-};
--- ast/graveyard/variable-declaration.js
+++
@@ -1,7 +0,0 @@
-// const Variable = require("./variable");
-
-module.exports = class VariableDeclaration {
-  constructor(type, ids, initializers) {
-    Object.assign(this, { type, ids, initializers });
-  }
-};
