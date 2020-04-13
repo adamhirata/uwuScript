a4392187560ca8ce87e4431faf2313be9a7f0242
Krysten Tachiyama <54608900+ktachiyama@users.noreply.github.com>
2020-04-13 00:10:48 -0700

    Created check.js

---
+++ semantics/check.js
@@ -0,0 +1,16 @@
+const { NumType, StringType, BooleanType } = require("./builtins");
+
+function doCheck(condition, message) {
+  if (!condition) {
+    throw new Error(message);
+  }
+}
+
+
+module.exports = {
+
+
+
+
+
+};
